cfc50a3782475a3ac68ee4a2b5b66f1ddf4fba7c
nisha027 <nishaguptag399@gmail.com>
2022-09-04 11:46:29 +0530

    first commit

---
+++ problem3.js
@@ -0,0 +1,10 @@
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name="Nisha Gupta";
+let age=21;
+console.log(name,age);
+console.log(typeof(name));
+console.log(typeof(age));
---
+++ problem4.js
@@ -0,0 +1,34 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+let name="Nisha Gupta";
+let school="Rajiv Gandhi College";
+let grade="+A";
+let section="A";
+let roll_no=0411;
+let math=78;
+let phy=80;
+let eng=91;
+let title="***************-REPORT CARD-***************";
+let line1="**                                       **";
+let line2="**";
+console.log(title);
+console.log(line1);
+console.log(line1);
+console.log(line2,name+"                           "+line2);
+
+console.log(line2,school+"                  "+line2);
+
+console.log(line2,grade+"                                    "+line2);
+console.log(line2,section +"                                     "+line2)
+console.log(line2,roll_no +"                                   "+line2)
+console.log(line2,math,phy,eng+"                              "+line2)
+
+
+console.log(line1);
+console.log(line1);
+console.log(title);
+
+
